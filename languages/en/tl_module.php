e613307ad399c78d609af1059bf0afde8392cb04
Jozef Dvorský <creatingo@users.noreply.github.com>
2013-09-23 16:58:06 +0200

    English translation update

--- languages/en/tl_module.php
+++ languages/en/tl_module.php
@@ -27,9 +27,10 @@ $GLOBALS['TL_LANG']['tl_module']['news4ward_readerTemplate'] = array('Entries re
 $GLOBALS['TL_LANG']['tl_module']['news4ward_format']        = array('Archive format', 'Here you can choose the news archive format.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_startDay']      = array('Week start day', 'Here you can choose the week start day.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_order']         = array('Sort order', 'Here you can choose the sort order.');
-$GLOBALS['TL_LANG']['tl_module']['news4ward_showQuantity']  = array('Entries per page', 'The number of entries per page. Set to 0 to disable pagination.');
+$GLOBALS['TL_LANG']['tl_module']['news4ward_showQuantity']  = array('Show quantity', 'Show the number of entries relative to the specific filter.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_facebookMeta']  = array('Create meta-data for Facebook', 'Facebook meta-data are atributes, which are used/shared on Facebook. What more Teaser image will be used for Facebook.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_filterHint']  	= array('Current filter indicator', 'The indicator and currently used/selected filter is shown on page by Insert-Tag {{news4ward::filter_hint}}. The value of this field (e.g. <em>Category:</em>) describes the actual filter (e.g. <em>Sport</em>). On the page will be visible <em>Category: Sport</em>.');
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint']  	= array('Time constraint', 'Hier können Sie die anzuzeigenden Beiträge auf ein Zeitintervall beschränken.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_overwriteArchiveJumpTo']  	= array('Overwrite the archive jumpto page', 'Choose a separate reader page.');
 $GLOBALS['TL_LANG']['tl_module']['news4ward_ignoreFilters']  	= array('Ignore filter', 'This item-list ignores all filters.');
 
@@ -53,3 +54,14 @@ $GLOBALS['tl_module']['news4ward_order_ref']	= array
 	'title ASC'		=> 'title ASC',
 	'title DESC'	=> 'title DESC'
 );
+
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['all'] = 'Alle Beiträge';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['cur_month'] = 'des laufenden Monats';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['cur_year'] = 'des laufenden Jahres';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_7'] = '- 1 Woche';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_14'] = '- 2 Wochen';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_30'] = '- 1 Monat';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_90'] = '- 3 Monate';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_180'] = '- 6 Monate';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_365'] = '- 1 Jahr';
+$GLOBALS['TL_LANG']['tl_module']['news4ward_timeConstraint_ref']['past_two'] = '- 2 Jahre';
